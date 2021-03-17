524e2ba0964738f086842da117e58d244c916803
Brian Hart <brian.hart@xylogix.net>
2021-03-17 08:46:17 -0400

    Create overload of IsBoundToMessageId that does not have a generic type parameter

--- xyLOGIX.Queues.Messages/MessageQueueItemExtensions.cs
+++ xyLOGIX.Queues.Messages/MessageQueueItemExtensions.cs
@@ -67,5 +67,30 @@ namespace xyLOGIX.Queues.Messages
             Guid messageId)
             => item != null && messageId != Guid.Empty &&
                item.EventDataType == typeof(T) && item.MessageId == messageId;
+
+        /// <summary>
+        /// Determines whether a message queue <paramref name="item" /> has data
+        /// of type <typeparamref name="T" /> and corresponds to the message ID
+        /// specified by the <paramref name="messageId" /> parameter.
+        /// </summary>
+        /// <param name="item">
+        /// Reference to an instance of an object that implements the
+        /// <see
+        ///     cref="T:SampleMVP.IMessageQueueItem" />
+        /// interface that represents the
+        /// message queue item to be checked.
+        /// </param>
+        /// <param name="messageId">
+        /// A <see cref="T:System.Guid" /> indicating who should receive the message.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if the message queue <paramref name="item" /> is bound
+        /// for the message ID with the specified <paramref name="messageId" />;
+        /// <c>false</c> otherwise.
+        /// </returns>
+        public static bool IsBoundToMessageId(this IMessageQueueItem item,
+            Guid messageId)
+            => item != null && messageId != Guid.Empty &&
+               item.MessageId == messageId;
     }
 }
