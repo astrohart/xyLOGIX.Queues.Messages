e6b881cb27737be86782b8fd8ab823ab85081323
Brian Hart <brian.hart@xylogix.net>
2021-03-17 08:43:40 -0400

    Cleanup code

--- xyLOGIX.Queues.Messages.Models/MessageQueueItem.cs
+++ xyLOGIX.Queues.Messages.Models/MessageQueueItem.cs
@@ -4,24 +4,23 @@ using xyLOGIX.Queues.Messages.Interfaces;
 namespace xyLOGIX.Queues.Messages.Models
 {
     /// <summary>
-    /// Represents a single item in a
-    /// <see cref="T:xyLOGIX.Queues.Messages.MessageQueue" />.
+    /// Represents a single item in a <see cref="T:xyLOGIX.Queues.Messages.MessageQueue"/>.
     /// </summary>
     public class MessageQueueItem : IMessageQueueItem
     {
         /// <summary>
-        /// An <see cref="T:System.Action" /> that specifies how to remove this
+        /// An <see cref="T:System.Action"/> that specifies how to remove this
         /// <c>MessageQueueItem</c> from the <c>MessageQueue</c>.
         /// </summary>
         private Action<IMessageQueueItem> _removalAction;
 
         /// <summary>
-        /// Gets or sets the <see cref="T:System.Type" /> of the event data.
+        /// Gets or sets the <see cref="T:System.Type"/> of the event data.
         /// </summary>
         public Type EventDataType { get; set; }
 
         /// <summary>
-        /// Gets or sets a <see cref="T:System.Delegate" /> that specifies what
+        /// Gets or sets a <see cref="T:System.Delegate"/> that specifies what
         /// code is to be run when a message is sent.
         /// </summary>
         public Delegate MessageHandler { get; set; }
@@ -44,7 +43,7 @@ namespace xyLOGIX.Queues.Messages.Models
         /// method, for fluent use.
         /// </returns>
         /// <exception cref="T:System.ArgumentNullException">
-        /// Thrown if the required parameter, <paramref name="messageHandler" />,
+        /// Thrown if the required parameter, <paramref name="messageHandler"/>,
         /// is passed a <c>null</c> value.
         /// </exception>
         public IMessageQueueItem AndHandler(Delegate messageHandler)
@@ -58,12 +57,19 @@ namespace xyLOGIX.Queues.Messages.Models
         }
 
         /// <summary>
+        /// Performs application-defined tasks associated with freeing,
+        /// releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+            => _removalAction?.Invoke(this);
+
+        /// <summary>
         /// Fluent-builder method to mark this <c>MessageQueueItem</c> for
         /// processing only by those objects who map handlers to a specific
         /// message ID.
         /// </summary>
         /// <param name="messageId">
-        /// (Required.) A <see cref="T:System.Guid" /> tagging this message with
+        /// (Required.) A <see cref="T:System.Guid"/> tagging this message with
         /// a unique identifier.
         /// </param>
         /// <returns>
@@ -73,7 +79,7 @@ namespace xyLOGIX.Queues.Messages.Models
         /// <remarks>
         /// Calling this method is optional. The Zero GUID will be associated
         /// with this <c>MessageQueueItem</c> if this method is not called.
-        /// <para />
+        /// <para/>
         /// Associating a message with the Zero GUID means that the message in
         /// question should be dispatched to all interested parties.
         /// </remarks>
@@ -85,16 +91,14 @@ namespace xyLOGIX.Queues.Messages.Models
         }
 
         /// <summary>
-        /// Fluent-builder method that enables us to attach a
-        /// <see
-        ///     cref="T:System.Action{xyLOGIX.Queues.Messages.Interfaces.IMessageQueueItem}" />
+        /// Fluent-builder method that enables us to attach a <see
+        /// cref="T:System.Action{xyLOGIX.Queues.Messages.Interfaces.IMessageQueueItem}"/>
         /// to this object that is called when this object's resources are freed
         /// from memory by the garbage collector.
-        /// <para />
-        /// The most common implementation is to remove the newly-built
-        /// <see cref="T:xyLOGIX.Queues.Messages.Interfaces.IMessageQueueItem" />
+        /// <para/>
+        /// The most common implementation is to remove the newly-built <see cref="T:xyLOGIX.Queues.Messages.Interfaces.IMessageQueueItem"/>
         /// -implementing object from the internal list maintained by the
-        /// <c>MessageQueue</c> object.
+        ///  <c>MessageQueue</c> object.
         /// </summary>
         /// <returns>
         /// Reference to the same instance of the object that called this
@@ -110,12 +114,5 @@ namespace xyLOGIX.Queues.Messages.Models
 
             return this;
         }
-
-        /// <summary>
-        /// Performs application-defined tasks associated with freeing,
-        /// releasing, or resetting unmanaged resources.
-        /// </summary>
-        public void Dispose()
-            => _removalAction?.Invoke(this);
     }
 }
