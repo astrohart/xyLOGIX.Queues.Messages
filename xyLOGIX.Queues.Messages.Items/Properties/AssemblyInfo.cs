e71059a37a857f901792d410b8e7944162b78d5f
Brian Hart <brian.hart@xylogix.net>
2023-06-22 19:59:02 -0600

    Create the 'xyLOGIX.Queues.Messages.Items' module
    Provides objects and functionality for the individual elements of a message queue.
    
    This module is a component of the xyLOGIX Support Library for Event Aggregation software system.
    
    ---
    Copyright © 2023 by xyLOGIX, LLC.  All rights reserved.

---
+++ xyLOGIX.Queues.Messages.Items/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("xyLOGIX Support Library for Support Library for Event Aggregation's Items in Message Queues Component")]
+[assembly: AssemblyDescription("Provides objects and functionality for the individual elements of a message queue.  This particular library holds the concrete classes and implementations of the component.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("xyLOGIX, LLC")]
+[assembly: AssemblyProduct("xyLOGIX Support Library for Support Library for Event Aggregation")]
+[assembly:
+    AssemblyCopyright(
+        "Copyright © 2023 by xyLOGIX, LLC.  All rights reserved.")]
+[assembly:
+    AssemblyTrademark(
+        "This software is a component of xyLOGIX Support Library for Event Aggregation.  xyLOGIX Support Library for Event Aggregation is a trademark of xyLOGIX, LLC.  All rights reserved, especially those granted by international treaties.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5ec8cf64-8243-4592-a8a4-be4f78b08887")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
