9c43e25b0d1ba70366a35177fe7627603415908d
Brian Hart <brian.hart@xylogix.net>
2023-06-22 19:34:34 -0600

    Create the 'xyLOGIX.Queues.Messages.Comparers' module
    Provides objects and functionality to support comparing two message queues, or individual message queue items, for equality with each other.
    
    This module is a component of the xyLOGIX Support Library for Event Aggregation software system.
    
    ---
    Copyright © 2023 by xyLOGIX, LLC.  All rights reserved.

---
+++ xyLOGIX.Queues.Messages.Comparers.Tests/README.md
@@ -0,0 +1,34 @@
+<a name='assembly'></a>
+# xyLOGIX.Queues.Messages.Comparers.Tests
+
+## Contents
+
+- [Resources](#T-xyLOGIX.Queues.Messages.Comparers.Tests-Properties-Resources 'xyLOGIX.Queues.Messages.Comparers.Tests.Properties.Resources')
+  - [Culture](#P-xyLOGIX.Queues.Messages.Comparers.Tests-Properties-Resources-Culture 'xyLOGIX.Queues.Messages.Comparers.Tests.Properties.Resources.Culture')
+  - [ResourceManager](#P-xyLOGIX.Queues.Messages.Comparers.Tests-Properties-Resources-ResourceManager 'xyLOGIX.Queues.Messages.Comparers.Tests.Properties.Resources.ResourceManager')
+
+<a name='T-xyLOGIX.Queues.Messages.Comparers.Tests-Properties-Resources'></a>
+## Resources `type`
+
+##### Namespace
+
+xyLOGIX.Queues.Messages.Comparers.Tests.Properties
+
+##### Summary
+
+A strongly-typed resource class, for looking up localized strings, etc.
+
+<a name='P-xyLOGIX.Queues.Messages.Comparers.Tests-Properties-Resources-Culture'></a>
+### Culture `property`
+
+##### Summary
+
+Overrides the current thread's CurrentUICulture property for all
+  resource lookups using this strongly typed resource class.
+
+<a name='P-xyLOGIX.Queues.Messages.Comparers.Tests-Properties-Resources-ResourceManager'></a>
+### ResourceManager `property`
+
+##### Summary
+
+Returns the cached ResourceManager instance used by this class.
---
+++ xyLOGIX.Queues.Messages.Comparers/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("xyLOGIX Support Library for Support Library for Event Aggregation's Equality Comparers for Message Queues and Message-Queue Items Component")]
+[assembly: AssemblyDescription("Provides objects and functionality to support comparing two message queues, or individual message queue items, for equality with each other.  This particular library holds the concrete classes and implementations of the component.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("xyLOGIX, LLC")]
+[assembly: AssemblyProduct("xyLOGIX Support Library for Support Library for Event Aggregation")]
+[assembly:
+    AssemblyCopyright(
+        "Copyright © 2023 by xyLOGIX, LLC.  All rights reserved.")]
+[assembly:
+    AssemblyTrademark(
+        "This software is a component of xyLOGIX Support Library for Event Aggregation.  xyLOGIX Support Library for Event Aggregation is a trademark of xyLOGIX, LLC.  All rights reserved, especially those granted by international treaties.")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("39512979-7c82-4390-b350-e3f9a75558da")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
