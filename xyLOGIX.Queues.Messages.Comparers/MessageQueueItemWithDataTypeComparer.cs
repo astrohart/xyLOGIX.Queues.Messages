b72b95ae92eec89f5cc12a3b4664757f69e2c7d1
Brian Hart <brian.hart@xylogix.net>
2025-09-22 00:26:51 -0600

    Update MessageQueueItemWithDataTypeComparer.cs
    - Replacing all occurrences of `object sender` and `EventArgs e`, respectively, are prefaced with `[NotLogged]`.  This is being done so as to ensure that the value(s) of these parameter(s) do not get written to PostSharp log file(s).  Also updated all auto-property(ies) to make sure their getters and setters are all prefixed with the `[DebuggerStepThrough]` attribute.

--- xyLOGIX.Queues.Messages.Comparers/MessageQueueItemWithDataTypeComparer.cs
+++ xyLOGIX.Queues.Messages.Comparers/MessageQueueItemWithDataTypeComparer.cs
@@ -1,4 +1,5 @@
-﻿using PostSharp.Patterns.Diagnostics;
+﻿using System.Diagnostics;
+using PostSharp.Patterns.Diagnostics;
 using System;
 using System.Collections.Generic;
 using xyLOGIX.Core.Debug;
