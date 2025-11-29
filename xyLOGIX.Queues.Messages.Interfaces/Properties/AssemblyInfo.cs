045050beadbc569e6f320fc85ce0559869dca1c6
Brian Hart <brian.hart@xylogix.net>
2025-11-29 01:09:14 -0700

    Update AssemblyInfo.cs
    * Standardized how we described the assembly.

--- xyLOGIX.Queues.Messages.Interfaces/Properties/AssemblyInfo.cs
+++ xyLOGIX.Queues.Messages.Interfaces/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
     AssemblyTitle(
         "xyLOGIX Support Library for xyLOGIX.Queues.Messages.Interfaces"
     )]
-[assembly: AssemblyDescription("Provides objects and functionality for ")]
+[assembly: AssemblyDescription("Provides object(s) and functionality for ")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("xyLOGIX, LLC")]
 [assembly:
